163901aff4a7a5523961dbde73ab508efd436eab
NicholasArner <33156299+NicholasArner@users.noreply.github.com>
2020-11-21 15:49:08 -0800

    added Browser class

---
+++ src/Browser.java
@@ -0,0 +1,19 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Browser extends AnimatedEntity{
+    protected Browser(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod) {
+        super(id, position, images, actionPeriod, animationPeriod);
+    }
+
+    @Override
+    protected void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore) {
+
+    }
+
+    @Override
+    protected void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+
+    }
+}
